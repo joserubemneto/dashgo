011f0b838382f7cfcd902311f3847bd026f1f527
Jose Rubem <joserubemn31@gmail.com>
2021-04-18 00:13:46 +0100

    feat: setup miragejs and react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,14 +13,25 @@ import {
   Td,
   Checkbox,
   useBreakpointValue,
+  Spinner,
 } from '@chakra-ui/react'
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useQuery } from 'react-query'
+
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UserList() {
+  const { data, isLoading, isError } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -48,47 +59,58 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color='gray.300' w='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>User</Th>
-                {isWideVersion && <Th>Registration date</Th>}
-                {isWideVersion && <Th w='8'></Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>José Rubem</Text>
-                    <Text fontSize='sm' color='gray.300'>
-                      joserubem@ua.pt
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>April 4 2021</Td>}
-                {isWideVersion && (
-                  <Td>
-                    <Button
-                      as='a'
-                      size='sm'
-                      fontSize='sm'
-                      colorScheme='purple'
-                      leftIcon={<Icon as={RiPencilLine} fontSize='16' />}>
-                      Edit
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+
+          {isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : isError ? (
+            <Flex justify='center'>Failed to get users</Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color='gray.300' w='8'>
+                      <Checkbox colorScheme='pink' />
+                    </Th>
+                    <Th>User</Th>
+                    {isWideVersion && <Th>Registration date</Th>}
+                    {isWideVersion && <Th w='8'></Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme='pink' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>José Rubem</Text>
+                        <Text fontSize='sm' color='gray.300'>
+                          joserubem@ua.pt
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>April 4 2021</Td>}
+                    {isWideVersion && (
+                      <Td>
+                        <Button
+                          as='a'
+                          size='sm'
+                          fontSize='sm'
+                          colorScheme='purple'
+                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}>
+                          Edit
+                        </Button>
+                      </Td>
+                    )}
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
