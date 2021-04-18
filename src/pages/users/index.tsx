e771b1a7328b0a7807e35e29b9b576f065deedae
Jose Rubem <joserubemn31@gmail.com>
2021-04-18 17:29:34 +0100

    feat: added useUsers hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,40 +16,15 @@ import {
   Spinner,
 } from '@chakra-ui/react'
 import Link from 'next/link'
-import { useEffect } from 'react'
-import { useQuery } from 'react-query'
 
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
+import { useUsers } from '../../services/hooks/useUsers'
 
 export default function UserList() {
-  const { data, isLoading, isError } = useQuery(
-    'users',
-    async () => {
-      const response = await fetch('http://localhost:3000/api/users')
-      const data = await response.json()
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          }),
-        }
-      })
-
-      return users
-    },
-    {
-      staleTime: 1000 * 5, // 5 seconds
-    }
-  )
+  const { data, isLoading, isError, isFetching } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -66,7 +41,11 @@ export default function UserList() {
           <Flex mb='8' justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'>
               Users
+              {!isLoading && isFetching && (
+                <Spinner size='sm' color='gray.500' ml='4' />
+              )}
             </Heading>
+
             <Link href='/users/create' passHref>
               <Button
                 as='a'
