9b3a336a94a488cabab196013e7045c8f45cd85c
Jose Rubem <joserubemn31@gmail.com>
2021-04-18 18:56:20 +0100

    feat: added onPageChange function

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,11 +3,13 @@ import { Button } from '@chakra-ui/react'
 interface PaginationItemProps {
   number: number
   isCurrent?: boolean
+  onPageChange: (page: number) => void
 }
 
 export function PaginationItem({
   isCurrent = false,
   number,
+  onPageChange,
 }: PaginationItemProps) {
   if (isCurrent) {
     return (
@@ -33,7 +35,8 @@ export function PaginationItem({
       bg='gray.700'
       _hover={{
         bg: 'gray.500',
-      }}>
+      }}
+      onClick={() => onPageChange(number)}>
       {number}
     </Button>
   )
