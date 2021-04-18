bfb6b06f94c044ba340ba14a9b95fbdfe55a3bf8
Jose Rubem <joserubemn31@gmail.com>
2021-04-18 17:08:46 +0100

    feat: added dynamic data with react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,12 +25,31 @@ import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UserList() {
-  const { data, isLoading, isError } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+  const { data, isLoading, isError } = useQuery(
+    'users',
+    async () => {
+      const response = await fetch('http://localhost:3000/api/users')
+      const data = await response.json()
 
-    return data
-  })
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+          }),
+        }
+      })
+
+      return users
+    },
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  )
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -80,32 +99,34 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>José Rubem</Text>
-                        <Text fontSize='sm' color='gray.300'>
-                          joserubem@ua.pt
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>April 4 2021</Td>}
-                    {isWideVersion && (
-                      <Td>
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}>
-                          Edit
-                        </Button>
+                  {data.map((user) => (
+                    <Tr>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink' />
                       </Td>
-                    )}
-                  </Tr>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>{user.name}</Text>
+                          <Text fontSize='sm' color='gray.300'>
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                      {isWideVersion && (
+                        <Td>
+                          <Button
+                            as='a'
+                            size='sm'
+                            fontSize='sm'
+                            colorScheme='purple'
+                            leftIcon={<Icon as={RiPencilLine} fontSize='16' />}>
+                            Edit
+                          </Button>
+                        </Td>
+                      )}
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
               <Pagination />
